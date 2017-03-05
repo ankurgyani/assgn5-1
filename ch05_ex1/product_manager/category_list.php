39f70e539627cdc6f127c06daccff860cf09fd9f
ankurgyani <arg58@njit.edu>
2017-03-05 15:34:10 -0500

    file updated

--- ch05_ex1/product_manager/category_list.php
+++ ch05_ex1/product_manager/category_list.php
@@ -1,13 +1,24 @@
 <?php include '../view/header.php'; ?>
 <main>
 
-    <h1>Category List</h1>
+   <h1>Category List</h1>
     <table>
         <tr>
             <th>Name</th>
             <th>&nbsp;</th>
-        </tr>        
-        <!-- add category rows here -->
+        </tr>
+        
+	<?php foreach ($categories as $category) : ?>
+            <tr>
+                <td><?php echo $category['categoryName']; ?></td>
+                <td><form action="delete_category.php" method="post">
+                    <input type="hidden" name="categoryID"
+                           value="<?php echo $category['categoryID']; ?>">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+            <?php endforeach; ?>
+
     </table>
 
     <h2>Add Category</h2>
